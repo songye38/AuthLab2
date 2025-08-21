2c0c9627f6a44bd8b5cb166dad3e7eafed3bca21
songye38 <ddev0308@gmail.com>
2025-08-21 20:39:50 +0900

    UPDATE me컴포넌트에서도 fetch -> axios로 변경

--- frontend/src/pages/Me.tsx
+++ frontend/src/pages/Me.tsx
@@ -3,49 +3,44 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import LoggedInView from "../components/LoggedInView";
 import LoggedOutView from "../components/LoggedOutView";
+import api from "../api/api"; // api 인스턴스 가져오기
 
 const Me = () => {
-    const { user, login, logout } = useAuth();
-    const location = useLocation();
-    const query = new URLSearchParams(location.search);
-    const loginSuccess = query.get("login"); // "success"인지 확인
+  const { user, login, logout } = useAuth();
+  const location = useLocation();
+  const query = new URLSearchParams(location.search);
+  const loginSuccess = query.get("login"); // "success"인지 확인
 
-    console.log("Me page user:", user, "Login success:", loginSuccess);
-
-
-
-useEffect(() => {
-  console.log("Fetching me page with cookie access_token...");
-
-  fetch("https://api.songyeserver.info/users/me", {
-    method: "GET",
-    credentials: "include", // access_token 쿠키 자동 포함
-  })
-    .then(res => {
-      if (!res.ok) throw new Error("Not authenticated");
-      return res.json();
-    })
-    .then(data => {
-      console.log("Me page fetch data:", data);
-      console.log("user",user);
-      login(data.name);
-    })
-    .catch(err => {
-      console.error(err);
-      logout();
-    });
-}, []);
+  console.log("Me page user:", user, "Login success:", loginSuccess);
 
 
-    return (
-        <>
-            {user ? (
-                <LoggedInView userName={user} loginSuccess={loginSuccess} />
-            ) : (
-                <LoggedOutView />
-            )}
-        </>
-    );
+
+  useEffect(() => {
+    console.log("Fetching me page with cookie access_token...");
+
+    api.get("/users/me") // withCredentials와 인터셉터 자동 적용
+      .then(res => {
+        console.log("Me page fetch data:", res.data);
+        login(res.data.name);
+        sessionStorage.setItem("userName", res.data.name); // 세션에도 저장
+      })
+      .catch(err => {
+        console.error("Failed to fetch user info:", err);
+        logout();
+        sessionStorage.removeItem("userName");
+      });
+  }, []);
+
+
+  return (
+    <>
+      {user ? (
+        <LoggedInView userName={user} loginSuccess={loginSuccess} />
+      ) : (
+        <LoggedOutView />
+      )}
+    </>
+  );
 };
 
 export default Me;
